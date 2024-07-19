0b9ffda3482d93eb2369715ed7b94a0569f09a7f
Adrien <adrien.parasote@gmail.com>
2024-07-19 18:23:21 +0200

    CSS update

--- src/html/card.html.ts
+++ src/html/card.html.ts
@@ -12,8 +12,7 @@ export const cardError = (entity) =>
     html`<p class="error">${entity} is unavailable.</p>`;
 
 export const cardHeader = (vacuum, imgSrc) =>
-    html`<div>
-        <img class="vacuum ${vacuum.state}" src="${imgSrc}" />
+    html`<img class="vacuum ${vacuum.state}" src="${imgSrc}" />
         <div class="properties">
             <div class="vacuum-name">${vacuum.name}</div>
             <div class="vacuum-state">${vacuum.attributes.state}</div>
@@ -21,8 +20,7 @@ export const cardHeader = (vacuum, imgSrc) =>
         <div class="battery">
             <ha-icon icon="${vacuum.attributes.battery_icon}"></ha-icon>
             <span class="icon-title">${vacuum.attributes.battery_level}%</span>
-        </div>
-    </div> `;
+        </div> `;
 
 export const cardContent = () => html` <div>todo</div>`;
 
